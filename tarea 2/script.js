98653691157f7a60c9283325403f6f399cb6b6db
Ebani Nazario Rivera <nazarioacalli@gmail.com>
2025-02-25 10:55:04 -0600

    tarea 2

--- tarea 2/script.js
+++ tarea 2/script.js
@@ -1,2 +1,5 @@
-document.getElementById("parrafo1").innerHTML="parrafo1";
-document.getElementById("parrafo2").innerHTML="parrafo2";
+let p1="Aqui comienza todo";
+let p2="parrafo 2 escribe aqui";
+
+document.getElementById("parrafo1").innerHTML=parrafo1;
+document.getElementById("parrafo2").innerHTML=parrafo34;
