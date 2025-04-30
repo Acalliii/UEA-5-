02b09d11c0af757bf26f4e81136a76dd9b77df20
Ebani Nazario Rivera <nazarioacalli@gmail.com>
2025-04-29 21:01:47 -0600

    Tarea 2

---
+++ tarea2/script.js
@@ -0,0 +1,10 @@
+let p1= "Considero que en una carrera tan tecnológica y digital como lo es Arte y Comunicaciones Digitales, el HTML, CSS y JavaScript, son la base para cualquier cosa que quiera realizar, el tener conocimientos de estas abre las puertas a crear cosas grandiosas y muy creativas. El ver el modo de uso de estas herramientas, las limitaciones y todo lo que se puede lograr sabiendo un poco de programación es algo que me gusta, me gusta poder ver este tipo de cosas tan interactivas y didácticas en las clases.";
+
+let p2= "Un contra que estas pueden llegar a tener principalmente son el idioma en el que se encuentran hechas, como la mayoría del contenido que se encuentra en internet es en el idioma de ingles, esta no es la excepción, aunque existe el auto corrector en algunas partes del código puede traducirse de manera errónea por lo que el mensaje y código inicial no es el mismo y creo que ese es un punto importante a tratar sobre estas tecnologías, pero en lo personal son grandes herramientas aun con este punto.";
+
+document.getElementById('parr1').innerHTML=p1;
+
+document.getElementById('parr2').innerHTML=p2;
+
+console.log(p1.length);
+console.log(p2.length);
